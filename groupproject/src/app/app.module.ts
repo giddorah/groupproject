209aa0894ec85a641e96a0d623b3ea953b01039c
woohaaa <woohaaa@gmail.com>
2017-11-15 12:03:27 +0100

    Lagt till admin och home

--- groupproject/src/app/app.module.ts
+++ groupproject/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { HomeComponent } from './components/home/home.component';
+import { AdminComponent } from './components/admin/admin.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    HomeComponent,
+    AdminComponent
   ],
   imports: [
     NgbModule.forRoot(),
