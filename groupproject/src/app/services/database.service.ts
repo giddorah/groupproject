1d480b001e9ee18d8f8f1bb7531a81dcafd581a5
woohaaa <woohaaa@gmail.com>
2017-11-15 16:04:50 +0100

    getbykey

--- groupproject/src/app/services/database.service.ts
+++ groupproject/src/app/services/database.service.ts
@@ -12,8 +12,8 @@ export class DatabaseService {
     firebase: any;
 
     constructor(private database: AngularFireDatabase) {
-        this.blogList = database.list("BlogCollection");
-        this.blogObject = database.object("BlogCollection");
+        this.blogList = database.list("collectionOfBlogs");
+        this.blogObject = database.object("collectionOfBlogs");
         this.firebase = firebase;
     }
 
@@ -30,7 +30,7 @@ export class DatabaseService {
 
     public getBlogs() {
 
-        return this.database.list("BlogCollection").valueChanges();
+        return this.database.list("collectionOfBlogs").valueChanges();
         
     }
 
@@ -38,7 +38,7 @@ export class DatabaseService {
     public getKeys() {
      
         const keyArray = [];
-        this.firebase.database().ref("BlogCollection").on("child_added", snapshot => {
+        this.firebase.database().ref("collectionOfBlogs").on("child_added", snapshot => {
             keyArray.push(snapshot.key);
         });
 
