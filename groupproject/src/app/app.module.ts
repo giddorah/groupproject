d475ad94c532b15b89d592766079cb4d400e4130
woohaaa <woohaaa@gmail.com>
2017-11-15 12:16:17 +0100

    Comments

--- groupproject/src/app/app.module.ts
+++ groupproject/src/app/app.module.ts
@@ -1,11 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
+
+// Extern
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+// Routes
+import { RouterModule, Routes } from '@angular/router';
+
+
 
 // Our routes
 const appRoutes = [
