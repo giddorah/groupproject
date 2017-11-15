353d8fd7b6c0ae154503776a0a990bb0c52f3cc0
Johan Spånberg <j.spanberg@live.com>
2017-11-15 13:25:37 +0100

    Test

--- groupproject/src/app/app.module.ts
+++ groupproject/src/app/app.module.ts
@@ -1,15 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
-<<<<<<< HEAD
-import { NgModule, NO_ERRORS_SCHEMA  } from '@angular/core';
-
-=======
 import { NgModule } from '@angular/core';
->>>>>>> aed0fe188d53886e79c6f85b37fdf87769178580
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { HomeComponent } from './components/home/home.component';
 import { AdminComponent } from './components/admin/admin.component';
 import { environment } from './../environments/environment';
+
 // Extern
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
