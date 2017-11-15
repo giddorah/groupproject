9a46b852bc87a8fcb2b1a178b3a04cef36988c92
Ludwig Kleiser <ludde_orby@hotmail.com>
2017-11-15 12:56:41 +0100

    Added db in enviroment

--- groupproject/src/environments/environment.prod.ts
+++ groupproject/src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyCEbDIc88YT_UuAIwXJF9GwftdGMlZQvGY',
+    authDomain: 'blogspot-2240c.firebaseapp.com',
+    databaseURL: 'https://blogspot-2240c.firebaseio.com',
+    projectId: 'blogspot-2240c',
+    storageBucket: 'blogspot-2240c.appspot.com',
+    messagingSenderId: '770819581699'
+  }
 };
