60a24dbeaa88dccc9adbeb75085b98058e291e92
Ludwig Kleiser <ludde_orby@hotmail.com>
2017-11-15 14:40:26 +0100

    Printing out databast stuff on homepage

--- groupproject/src/app/components/home/home.component.ts
+++ groupproject/src/app/components/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
-
+import { DatabaseService } from './../../services/database.service';
+import { Observable } from 'rxjs/Observable';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  databaseService: any;
+  blogCollections: Observable<any>;
 
-  constructor() { }
 
-  ngOnInit() {
+  constructor(
+    databaseService: DatabaseService
+  ) {
+    this.databaseService = databaseService;
   }
 
+  ngOnInit() {
+    this.blogCollections = this.databaseService.getBlogs();
+  }
 }
