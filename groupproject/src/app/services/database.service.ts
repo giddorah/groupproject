fdcfc506aacab90dd623d21fda73b1831b293204
Johan Spånberg <j.spanberg@live.com>
2017-11-20 12:28:56 +0100

    Added commentary to explain some code.

--- groupproject/src/app/services/database.service.ts
+++ groupproject/src/app/services/database.service.ts
@@ -6,52 +6,55 @@ import * as firebase from 'firebase/app';
 @Injectable()
 export class DatabaseService {
 
-    blogList: AngularFireList<Blog>;
-    blogObject: AngularFireObject<Blog>;
+    blogList: AngularFireList<Blog>; // A list of AngularFireList with Blog objects
+    blogObject: AngularFireObject<Blog>; // A blog object
 
-    firebase: any;
+    firebase: any; // The database
 
     constructor(private database: AngularFireDatabase) {
-        this.blogList = database.list('collectionOfBlogs');
+        // The constructor adds the data to the above variables from the database.
+        this.blogList = database.list('collectionOfBlogs'); 
         this.blogObject = database.object('collectionOfBlogs');
         this.firebase = firebase;
     }
 
     public addBlog(blog: Blog) {
+        // Pushes the contents from bloglist to blog.
         this.blogList.push(blog);
     }
 
     public deleteBlog(key: string) {
-
+        // Not functional yet.
         this.blogList.remove(key);
     }
 
     public getBlogs() {
-
+        // Gets all the blogs and lists (returns) them.
         return this.database.list('collectionOfBlogs').valueChanges();
     }
 
 
     public getKeys() {
-
-        const keyArray = [];
+        // Gets all the unique keys from the blog database.
+        const keyArray = []; // Empty array which will be filled with keys
         this.firebase.database().ref("BlogCollection").on("child_added", snapshot => {
-            keyArray.push(snapshot.key);
+            keyArray.push(snapshot.key); // Gathers key-data from BlogCollection each time a child is added
         });
 
-        return keyArray;
+        return keyArray; // Returns the array of keys gathered from above
     }
 
     getBlogsAndKey() {
-
+        // Returns the blogs and the unique keys that each post has.
         return this.blogList.snapshotChanges().map(changes => {
             return changes.map(c => ({
-                key: c.payload.key, ...c.payload.val()
+                key: c.payload.key, ...c.payload.val() // "..." makes a copy of payload and adds the rest of the data.
             }));
         });
     }
 
     public getBlogByKey(key: string) {
+        // Gets the unique key of the clicked "read more" post
         return this.database.object('collectionOfBlogs/' + key).valueChanges();
     }
 }
